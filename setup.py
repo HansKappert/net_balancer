24c90bad2fc94803ed4b44d94b2488f2218f9757
Hans kappert <hans.kappert@hetconsultancyhuis.nl>
2021-11-24 20:11:11 +0100

    more tags filled

--- setup.py
+++ setup.py
@@ -2,8 +2,14 @@ from setuptools import setup
 
 setup(
     name='net_balancer',
+    version='0.1.0',
+    author='Hans Kappert',
+    author_email='hans.kappert@gmaill.com',
+   
     packages=['net_balancer','service'],
     include_package_data=True,
+    description='A program that sends purplus power to your Teslq',
+    long_description=open('README.md').read(),  
     install_requires=[
         'flask',
     ],
