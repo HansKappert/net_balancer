c2fb8c7618b4ce6cdede4788f99e43e7da524a35
Hans kappert <hans.kappert@hetconsultancyhuis.nl>
2022-11-25 08:09:48 +0100

    loggin

--- src/web.py
+++ src/web.py
@@ -86,6 +86,7 @@ def download_db_file():
 def download_csv_file():
     tmp_path = tempfile.gettempdir()
     file_name = os.path.join(tmp_path, "stats.csv")
+    logger.info(f"Writing csv file to: {file_name}")
     stats_retention_days = db.get_stats_retention()
     data = db.get_history(stats_retention_days * 24 * 60)
     with  open(file_name, "w") as f: 
@@ -94,7 +95,6 @@ def download_csv_file():
             dt = datetime.fromtimestamp(int(row[0])).strftime('%Y-%m-%d %H:%M:%S')
             f.write(f"{dt};{row[1]};{row[2]};{row[3]};{row[4]};{row[5]};{row[6]};{row[7]};\n")
         f.close()
-
     return send_file(file_name, as_attachment=True)
 
 
