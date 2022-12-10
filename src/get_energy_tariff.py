b96ea6e8d5adb39a79e89d9968671def54ffd39f
Hans kappert <hans.kappert@hetconsultancyhuis.nl>
2022-12-10 14:09:15 +0100

    demo price retrieve

--- src/get_energy_tariff.py
+++ src/get_energy_tariff.py
@@ -3,7 +3,7 @@ import urllib.parse
 import json
 
 from datetime import datetime, timedelta
-from_date  = datetime(2022,10,31,0,0,0,0) # price van 31 okt.
+from_date  = datetime(2022,12,6,23,0,0,0) # price van 31 okt.
 end_date   = from_date + timedelta(hours=23)
 base_url   = "https://api.energyzero.nl/v1/energyprices"
 url_params = "?fromDate={}&tillDate={}&interval=4&usageType=1&inclBtw=true"
